26a681a03715dd96eff7f1eef720ef678a7f66f8
TehGM <TehGM@users.noreply.github.com>
2024-10-28 18:54:36 +0000

    Add customer entity

---
+++ PinewoodTest.API/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace PinewoodTest.API
+{
+    public class Customer
+    {
+        public Guid Id { get; }
+
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string Address { get; set; }
+        public required string City { get; set; }
+
+        public Customer(Guid id)
+        {
+            this.Id = id;
+        }
+    }
+}
