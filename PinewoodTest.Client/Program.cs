214437919c6c26a6b62fb904046348f05bf503af
TehGM <TehGM@users.noreply.github.com>
2024-10-28 19:39:04 +0000

    Add Client project

---
+++ PinewoodTest.Client/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace PinewoodTest.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            // this HttpClient base address ideally wouldn't be hardcoded, but just to make this task a tad simpler...
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5245/api") });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
